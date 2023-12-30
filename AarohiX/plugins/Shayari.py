957b05c8ad25dd19bcd008e8c560e6f5767075be
Ꮥʜꫝʟɪɴɪ <150043650+Shalini77986@users.noreply.github.com>
2023-12-30 10:59:35 +0530

    Update Shayari.py

--- AarohiX/plugins/Shayari.py
+++ AarohiX/plugins/Shayari.py
@@ -86,9 +86,9 @@ async def help(client: Client, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        "✨sᴜᴘᴘᴏʀᴛ✨", url=f"https://t.me/LOVE_FEELING_WILL1"),
+                        "✨sᴜᴘᴘᴏʀᴛ✨", url=f"https://t.me/MUSIC_WORLD_SH"),
                     InlineKeyboardButton(
-                        "✨ᴏᴡɴᴇʀ✨", url=f"https://t.me/HONEY_SINGH_121")
+                        "✨ᴏᴡɴᴇʀ✨", url=f"https://t.me/shalini_shalu_69")
                     
                 ]
             ]
